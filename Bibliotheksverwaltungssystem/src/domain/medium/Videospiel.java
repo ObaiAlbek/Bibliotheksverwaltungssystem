5ee223b3266f542725a898a2f7756404a12f846a
Obai Albek <89144251+ObaiAlbek@users.noreply.github.com>
2025-09-28 16:52:57 +0200

    Update Videospiel.java

--- Bibliotheksverwaltungssystem/src/domain/medium/Videospiel.java
+++ Bibliotheksverwaltungssystem/src/domain/medium/Videospiel.java
@@ -1,31 +1,60 @@
 package domain.medium;
 
-import java.util.Date;
-
+/**
+ * Medientyp Videospiel mit Plattform.
+ * Erbt Grunddaten von {@link Medium}.
+ * Leere Plattform wird als "-" gespeichert.
+ *
+ * author Obai
+ * version 1.0
+ * since 1.0
+ */
 public class Videospiel extends Medium {
-	
-	private String plattform;
-	
-	public Videospiel(String ID,String title, int erscheinungsjahr, String plattform) {
-		super(ID,title, erscheinungsjahr);
-		
-		if (plattform.isEmpty())
-			this.plattform = "-";
-		else
-			this.plattform = plattform;
-	}
 
-	public String getPlattform() {
-		return plattform;
-	}
+    /** Zielplattform des Videospiels. */
+    private String plattform;
 
-	public void setPlattform(String plattform) {
-		this.plattform = plattform;
-	}
-	
-	@Override
-	public String toString() {
-		return "Videospiel: " + super.toString() +  " ,Plattform=" + plattform;
-	}
+    /**
+     * Konstruktor.
+     *
+     * @param ID eindeutige ID
+     * @param title Titel
+     * @param erscheinungsjahr Erscheinungsjahr
+     * @param plattform Plattform oder leer
+     */
+    public Videospiel(String ID, String title, int erscheinungsjahr, String plattform) {
+        super(ID, title, erscheinungsjahr);
+        if (plattform.isEmpty())
+            this.plattform = "-";
+        else
+            this.plattform = plattform;
+    }
 
+    /**
+     * Liefert die Plattform.
+     *
+     * @return Plattform
+     */
+    public String getPlattform() {
+        return plattform;
+    }
+
+    /**
+     * Setzt die Plattform.
+     *
+     * @param plattform neue Plattform
+     */
+    public void setPlattform(String plattform) {
+        this.plattform = plattform;
+    }
+
+    /**
+     * String-Repräsentation.
+     *
+     * @return Darstellung des Videospiels
+     */
+    @Override
+    public String toString() {
+        return "Videospiel: " + super.toString() + " ,Plattform=" + plattform;
+    }
 }
