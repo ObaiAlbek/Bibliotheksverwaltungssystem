f0ed17e7e47ac3e73844dd7d0d1b57c65ef99b2f
Obai Albek <89144251+ObaiAlbek@users.noreply.github.com>
2025-09-28 16:38:39 +0200

    Update Ausleihe.java

--- Bibliotheksverwaltungssystem/src/domain/ausleihSystem/Ausleihe.java
+++ Bibliotheksverwaltungssystem/src/domain/ausleihSystem/Ausleihe.java
@@ -1,65 +1,132 @@
 package domain.ausleihSystem;
 
 import java.time.LocalDate;
-import java.util.Date;
-
 import domain.benutzer.Benutzer;
 import domain.medium.Mediumverwalter;
 
+/**
+ * Repräsentiert eine Ausleihe in der Bibliothe.
+ *
+ * Enthält Informationen über das ausgeliehene Medium, den Benutzer, 
+ * den Beginn und das Ende der Ausleihe sowie die Anzahl der Verlängerungen.
+ *
+ */
 public class Ausleihe {
-	
-	private LocalDate ausleiheBeginn, ausleiheEnde;
-	private Mediumverwalter medium;
-	private Benutzer benutzer;
-	private int verlängerungen;
-	
-	public Ausleihe(Mediumverwalter medium, Benutzer benutzer,LocalDate ausleiheBeginn, LocalDate ausleiheEnde) {
-		this.medium = medium;
-		this.ausleiheBeginn = ausleiheBeginn;
-		this.ausleiheEnde = ausleiheEnde;
-		this.verlängerungen = 0;
-		this.benutzer = benutzer;
-	}
+    
+    /** Startdatum der Ausleihe. */
+    private LocalDate ausleiheBeginn;
 
-	public Mediumverwalter getMediumverwalter() {
-		return medium;
-	}
+    /** Enddatum der Ausleihe. */
+    private LocalDate ausleiheEnde;
 
-	public Benutzer getBenutzer() {
-		return benutzer;
-	}
+    /** Das ausgeliehene Medium. */
+    private Mediumverwalter medium;
 
-	public LocalDate getAusleiheBeginn() {
-		return ausleiheBeginn;
-	}
+    /** Der Benutzer, der das Medium ausgeliehen hat. */
+    private Benutzer benutzer;
 
-	public void setAusleiheBeginn(LocalDate ausleiheBeginn) {
-		this.ausleiheBeginn = ausleiheBeginn;
-	}
+    /** Anzahl der bereits durchgeführten Verlängerungen. */
+    private int verlängerungen;
 
-	public LocalDate getAusleiheEnde() {
-		return ausleiheEnde;
-	}
+    /**
+     * Erstellt eine neue Ausleihe für ein Medium und einen Benutzer.
+     *
+     * @param medium das ausgeliehene Medium
+     * @param benutzer der Benutzer, der das Medium ausleiht
+     * @param ausleiheBeginn das Startdatum der Ausleihe
+     * @param ausleiheEnde das Enddatum der Ausleihe
+     */
+    public Ausleihe(Mediumverwalter medium, Benutzer benutzer, LocalDate ausleiheBeginn, LocalDate ausleiheEnde) {
+        this.medium = medium;
+        this.ausleiheBeginn = ausleiheBeginn;
+        this.ausleiheEnde = ausleiheEnde;
+        this.verlängerungen = 0;
+        this.benutzer = benutzer;
+    }
 
-	public void setAusleiheEnde(LocalDate ausleiheEnde) {
-		this.ausleiheEnde = ausleiheEnde;
-	}
-	
-	public int getVerlängerungen() {
-		return verlängerungen;
-	}
+    /**
+     * Liefert das ausgeliehene Medium.
+     *
+     * @return das Medium
+     */
+    public Mediumverwalter getMediumverwalter() {
+        return medium;
+    }
 
-	public void setVerlängerungen(int verlängerungen) {
-		this.verlängerungen = verlängerungen;
-	}
-	
+    /**
+     * Liefert den Benutzer, der das Medium ausgeliehen hat.
+     *
+     * @return der Benutzer
+     */
+    public Benutzer getBenutzer() {
+        return benutzer;
+    }
 
-	@Override
-	public String toString() {
-		return "Ausgeliehene Mediums= "+ medium.toString()+ 
-				" Ausleihe Beginn= " +ausleiheBeginn + 
-				" ,Ausleihe Ende= " + ausleiheEnde + 
-				" ,verlängerungen=  " + verlängerungen + 
-				" ,inhaber= " + benutzer.toString() ;
-	}
+    /**
+     * Liefert das Startdatum der Ausleihe.
+     *
+     * @return Beginn der Ausleihe
+     */
+    public LocalDate getAusleiheBeginn() {
+        return ausleiheBeginn;
+    }
+
+    /**
+     * Setzt das Startdatum der Ausleihe.
+     *
+     * @param ausleiheBeginn neues Startdatum
+     */
+    public void setAusleiheBeginn(LocalDate ausleiheBeginn) {
+        this.ausleiheBeginn = ausleiheBeginn;
+    }
+
+    /**
+     * Liefert das Enddatum der Ausleihe.
+     *
+     * @return Ende der Ausleihe
+     */
+    public LocalDate getAusleiheEnde() {
+        return ausleiheEnde;
+    }
+
+    /**
+     * Setzt das Enddatum der Ausleihe.
+     *
+     * @param ausleiheEnde neues Enddatum
+     */
+    public void setAusleiheEnde(LocalDate ausleiheEnde) {
+        this.ausleiheEnde = ausleiheEnde;
+    }
+
+    /**
+     * Liefert die Anzahl der Verlängerungen.
+     *
+     * @return Anzahl der Verlängerungen
+     */
+    public int getVerlängerungen() {
+        return verlängerungen;
+    }
+
+    /**
+     * Setzt die Anzahl der Verlängerungen.
+     *
+     * @param verlängerungen neue Anzahl der Verlängerungen
+     */
+    public void setVerlängerungen(int verlängerungen) {
+        this.verlängerungen = verlängerungen;
+    }
+
+    /**
+     * Gibt eine String-Darstellung der Ausleihe zurück.
+     *
+     * @return String mit Informationen über Medium, Benutzer, Zeitraum und Verlängerungen
+     */
+    @Override
+    public String toString() {
+        return "Ausgeliehenes Medium= " + medium.toString() + 
+               " | Ausleihe Beginn= " + ausleiheBeginn + 
+               " | Ausleihe Ende= " + ausleiheEnde + 
+               " | Verlängerungen= " + verlängerungen + 
+               " | Inhaber= " + benutzer.toString();
+    }
 }
