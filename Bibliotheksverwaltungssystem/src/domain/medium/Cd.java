e94da22b95c160467f72f73e2649d0b292b18327
Obai Albek <89144251+ObaiAlbek@users.noreply.github.com>
2025-09-28 16:52:09 +0200

    Update Cd.java

--- Bibliotheksverwaltungssystem/src/domain/medium/Cd.java
+++ Bibliotheksverwaltungssystem/src/domain/medium/Cd.java
@@ -1,30 +1,60 @@
 package domain.medium;
 
-import java.util.Date;
-
+/**
+ * Medientyp CD mit Künstler.
+ * Erbt Grunddaten von {@link Medium}.
+ * Leerer Künstler wird als "-" gespeichert.
+ *
+ * author Obai
+ * version 1.0
+ * since 1.0
+ */
 public class Cd extends Medium {
-	
-	private String Künstler;
-	
-	public Cd(String ID,String title, int erscheinungsjahr,String Künstler) {
-		super(ID,title, erscheinungsjahr);
-		
-		if (Künstler.isEmpty())
-			this.Künstler = "-";
-		else
-			this.Künstler = Künstler;
-	}
-	
-	public String getKünstler() {
-		return Künstler;
-	}
-	public void setKünstler(String künstler) {
-		Künstler = künstler;
-	}
-	
-	@Override
-	public String toString() {
-		return "CD: " + super.toString() +  " ,Künstler=" + Künstler;
-	}
 
+    /** Künstler der CD. */
+    private String Künstler;
+
+    /**
+     * Konstruktor.
+     *
+     * @param ID eindeutige ID
+     * @param title Titel
+     * @param erscheinungsjahr Erscheinungsjahr
+     * @param Künstler Künstler oder leer
+     */
+    public Cd(String ID, String title, int erscheinungsjahr, String Künstler) {
+        super(ID, title, erscheinungsjahr);
+        if (Künstler.isEmpty())
+            this.Künstler = "-";
+        else
+            this.Künstler = Künstler;
+    }
+
+    /**
+     * Liefert den Künstler.
+     *
+     * @return Künstler
+     */
+    public String getKünstler() {
+        return Künstler;
+    }
+
+    /**
+     * Setzt den Künstler.
+     *
+     * @param künstler neuer Künstler
+     */
+    public void setKünstler(String künstler) {
+        Künstler = künstler;
+    }
+
+    /**
+     * String-Repräsentation.
+     *
+     * @return Darstellung der CD
+     */
+    @Override
+    public String toString() {
+        return "CD: " + super.toString() + " ,Künstler=" + Künstler;
+    }
 }
