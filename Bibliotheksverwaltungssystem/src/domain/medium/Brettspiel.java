0b69101ddca8f3498ceb6fe8989f6a6e7128cc6d
Obai Albek <89144251+ObaiAlbek@users.noreply.github.com>
2025-09-28 16:51:40 +0200

    Update Brettspiel.java

--- Bibliotheksverwaltungssystem/src/domain/medium/Brettspiel.java
+++ Bibliotheksverwaltungssystem/src/domain/medium/Brettspiel.java
@@ -1,30 +1,60 @@
 package domain.medium;
 
-
+/**
+ * Medientyp Brettspiel mit Verlag.
+ * Erbt Grunddaten von {@link Medium}.
+ * Leerer Verlag wird als "-" gespeichert.
+ * 
+ * author Obai
+ * version 1.0
+ * since 1.0
+ */
 public class Brettspiel extends Medium {
-	
-	private String Verlag;
 
-	public Brettspiel(String ID,String title, int erscheinungsjahr, String Verlag) {
-		super(ID,title, erscheinungsjahr);
-		
-		if (Verlag.isEmpty())
-			this.Verlag = "-";
-		else
-			this.Verlag = Verlag;
-	}
+    /** Verlag des Brettspiels. */
+    private String Verlag;
 
-	public String getVerlag() {
-		return Verlag;
-	}
+    /**
+     * Konstruktor.
+     *
+     * @param ID eindeutige ID
+     * @param title Titel
+     * @param erscheinungsjahr Erscheinungsjahr
+     * @param Verlag Verlag oder leer
+     */
+    public Brettspiel(String ID, String title, int erscheinungsjahr, String Verlag) {
+        super(ID, title, erscheinungsjahr);
+        if (Verlag.isEmpty())
+            this.Verlag = "-";
+        else
+            this.Verlag = Verlag;
+    }
 
-	public void setVerlag(String verlag) {
-		Verlag = verlag;
-	}
-	
-	@Override
-	public String toString() {
-		return "Brettspiel: " + super.toString() +  " ,Verlag=" + Verlag;
-	}
+    /**
+     * Liefert den Verlag.
+     *
+     * @return Verlag
+     */
+    public String getVerlag() {
+        return Verlag;
+    }
 
+    /**
+     * Setzt den Verlag.
+     *
+     * @param verlag neuer Verlag
+     */
+    public void setVerlag(String verlag) {
+        Verlag = verlag;
+    }
+
+    /**
+     * String-Repräsentation.
+     *
+     * @return Darstellung des Brettspiels
+     */
+    @Override
+    public String toString() {
+        return "Brettspiel: " + super.toString() + " ,Verlag=" + Verlag;
+    }
 }
