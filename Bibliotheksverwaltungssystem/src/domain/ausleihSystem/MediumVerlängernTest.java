8eea19d92b93b0af9aa074fe64d744fad47cf192
Obai Albek <89144251+ObaiAlbek@users.noreply.github.com>
2025-09-28 16:45:01 +0200

    Update MediumVerlängernTest.java

--- Bibliotheksverwaltungssystem/src/domain/ausleihSystem/MediumVerlängernTest.java
+++ Bibliotheksverwaltungssystem/src/domain/ausleihSystem/MediumVerlängernTest.java
@@ -1,27 +1,46 @@
 package domain.ausleihSystem;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import domain.fassade.BibSystem;
 
+/**
+ * Testet die Verlängerung einer bestehenden Ausleihe.
+ * 
+ * author Obai
+ * since 1.0
+ */
 class MediumVerlängernTest {
 
-	private BibSystem bib;
+    /** Fassade des Bibliothekssystems für End-to-End-Tests. */
+    private BibSystem bib;
 
-	@BeforeEach
-	void setUp() throws Exception {
-		this.bib = new BibSystem();
-	}
+    /**
+     * Initialisiert ein frisches System vor jedem Test.
+     *
+     * @throws Exception wenn Initialisierung fehlschlägt
+     */
+    @BeforeEach
+    void setUp() throws Exception {
+        this.bib = new BibSystem();
+    }
 
-	@Test
-	void test() throws Exception {
-		bib.userRegistrieren("obai", "student", 15, "nein");
-		bib.userAnmelden("K1001");
-		bib.mediumAusleihen("K1001", "B001");
-		assertTrue(bib.medienVerlängern("B001", "K1001"));
-	}
+    /**
+     * Prüft, dass eine Verlängerung unter gültigen Bedingungen erfolgreich ist.
+     *
+     * @throws Exception wenn der Ablauf fehlschlägt
+     */
+    @Test
+    @DisplayName("Verlängerung einer aktiven, verlängerbaren Ausleihe")
+    void verlaengerungErfolgreich() throws Exception {
+        bib.userRegistrieren("obai", "student", 15, "nein");
+        bib.userAnmelden("K1001");
+        bib.mediumAusleihen("K1001", "B001");
 
+        assertTrue(bib.medienVerlängern("B001", "K1001"));
+    }
 }
