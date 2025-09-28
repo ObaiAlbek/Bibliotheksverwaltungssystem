d0e34a21d1468af74b2ba232b00b4b9c3e2cb63b
Obai Albek <89144251+ObaiAlbek@users.noreply.github.com>
2025-09-28 16:47:01 +0200

    Update Student.java

--- Bibliotheksverwaltungssystem/src/domain/benutzer/Student.java
+++ Bibliotheksverwaltungssystem/src/domain/benutzer/Student.java
@@ -1,14 +1,34 @@
 package domain.benutzer;
 
+/**
+ * Repräsentiert einen Studenten als Bibliotheksbenutzer.
+ * Jahresgebühr beträgt 25 EUR.
+ *
+ * @author Obai
+ * @version 1.0
+ * @since 1.0
+ */
 public class Student extends Benutzer {
 
-	public Student(Ausweis bibAusweis, String name, int alter, boolean istStudent) {
-		super(bibAusweis, name, alter, istStudent);
-	}
+    /**
+     * Erstellt einen neuen Studenten-Benutzer.
+     *
+     * @param bibAusweis Bibliotheksausweis
+     * @param name Name des Studenten
+     * @param alter Alter des Studenten
+     * @param istStudent Student-Status (normalerweise true)
+     */
+    public Student(Ausweis bibAusweis, String name, int alter, boolean istStudent) {
+        super(bibAusweis, name, alter, istStudent);
+    }
 
-	@Override
-	public double getJahresgebühren() {
-		return 25.0;
-	}
-
+    /**
+     * Liefert die Jahresgebühren für Studenten.
+     *
+     * @return 25.0 EUR
+     */
+    @Override
+    public double getJahresgebühren() {
+        return 25.0;
+    }
 }
