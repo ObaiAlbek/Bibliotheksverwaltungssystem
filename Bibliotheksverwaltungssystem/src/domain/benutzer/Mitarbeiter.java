ea59650ddadb73eceba50d7536ab0f2ea130d9a2
Obai Albek <89144251+ObaiAlbek@users.noreply.github.com>
2025-09-28 16:46:50 +0200

    Update Mitarbeiter.java

--- Bibliotheksverwaltungssystem/src/domain/benutzer/Mitarbeiter.java
+++ Bibliotheksverwaltungssystem/src/domain/benutzer/Mitarbeiter.java
@@ -1,18 +1,44 @@
 package domain.benutzer;
 
+/**
+ * Repräsentiert einen Mitarbeiter der Bibliothek.
+ * Mitarbeiter zahlen keine Jahresgebühr.
+ * Zusätzlich können Mitarbeiter Gebühren für andere Benutzer verbuchen.
+ *
+ * @author Obai
+ * @version 1.0
+ * @since 1.0
+ */
 public class Mitarbeiter extends Benutzer {
 
-	public Mitarbeiter(Ausweis bibAusweis, String name, int alter, boolean istStudent) {
-		super(bibAusweis, name, alter, istStudent);
-	}
+    /**
+     * Erstellt einen neuen Mitarbeiter.
+     *
+     * @param bibAusweis Bibliotheksausweis
+     * @param name Name des Mitarbeiters
+     * @param alter Alter des Mitarbeiters
+     * @param istStudent Student-Status (meist irrelevant für Mitarbeiter)
+     */
+    public Mitarbeiter(Ausweis bibAusweis, String name, int alter, boolean istStudent) {
+        super(bibAusweis, name, alter, istStudent);
+    }
 
-	@Override
-	public double getJahresgebühren() {
-	
-		return 0.0;
-	}
-	
-	 public void gebührVerbuchen(Benutzer benutzer) {
-	        benutzer.setGebühren(0.0); 
-	 }
+    /**
+     * Liefert die Jahresgebühren für Mitarbeiter.
+     *
+     * @return 0.0 EUR
+     */
+    @Override
+    public double getJahresgebühren() {
+        return 0.0;
+    }
+
+    /**
+     * Verbucht alle offenen Gebühren eines Benutzers auf 0.0.
+     *
+     * @param benutzer Benutzer, dessen Gebühren zurückgesetzt werden sollen
+     */
+    public void gebührVerbuchen(Benutzer benutzer) {
+        benutzer.setGebühren(0.0);
+    }
 }
