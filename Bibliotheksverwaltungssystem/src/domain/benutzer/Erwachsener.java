d8eb1554a4928a670022374b7e0c048533f9f227
Obai Albek <89144251+ObaiAlbek@users.noreply.github.com>
2025-09-28 16:46:40 +0200

    Update Erwachsener.java

--- Bibliotheksverwaltungssystem/src/domain/benutzer/Erwachsener.java
+++ Bibliotheksverwaltungssystem/src/domain/benutzer/Erwachsener.java
@@ -1,14 +1,34 @@
 package domain.benutzer;
 
+/**
+ * Repräsentiert einen erwachsenen Bibliotheksbenutzer.
+ * Jahresgebühr beträgt 50 EUR.
+ *
+ * @author Obai
+ * @version 1.0
+ * @since 1.0
+ */
 public class Erwachsener extends Benutzer {
 
-	public Erwachsener(Ausweis bibAusweis, String name, int alter, boolean istStudent) {
-		super(bibAusweis, name, alter, istStudent);
-	}
+    /**
+     * Erstellt einen neuen Erwachsenen-Benutzer.
+     *
+     * @param bibAusweis Bibliotheksausweis
+     * @param name Name des Benutzers
+     * @param alter Alter des Benutzers
+     * @param istStudent Student-Status (normalerweise false für Erwachsene)
+     */
+    public Erwachsener(Ausweis bibAusweis, String name, int alter, boolean istStudent) {
+        super(bibAusweis, name, alter, istStudent);
+    }
 
-	@Override
-	public double getJahresgebühren() {
-		return 50.0;
-	}
-
+    /**
+     * Liefert die Jahresgebühren für Erwachsene.
+     *
+     * @return 50.0 EUR
+     */
+    @Override
+    public double getJahresgebühren() {
+        return 50.0;
+    }
 }
