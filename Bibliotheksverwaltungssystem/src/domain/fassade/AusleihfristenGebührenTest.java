bb3aaac918b2a9a90bd025f1d5ede08669abcf1c
Obai Albek <89144251+ObaiAlbek@users.noreply.github.com>
2025-09-28 16:57:32 +0200

    Update AusleihfristenGebührenTest.java

--- Bibliotheksverwaltungssystem/src/domain/fassade/AusleihfristenGebührenTest.java
+++ Bibliotheksverwaltungssystem/src/domain/fassade/AusleihfristenGebührenTest.java
@@ -1,25 +1,42 @@
 package domain.fassade;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+/**
+ * End-to-End-Test: Ausleihe starten ohne Gebührenerhöhung bei nicht überfälliger Frist.
+ *
+ * author Obai
+ * since 1.0
+ */
 class AusleihfristenGebührenTest {
 
-	private BibSystem bib;
+    /** Bibliothekssystem-Fassade. */
+    private BibSystem bib;
 
-	@BeforeEach
-	void setUp() throws Exception {
-		this.bib = new BibSystem();
-	}
+    /**
+     * Initialisiert ein frisches System vor jedem Test.
+     *
+     * @throws Exception wenn Initialisierung fehlschlägt
+     */
+    @BeforeEach
+    void setUp() throws Exception {
+        this.bib = new BibSystem();
+    }
 
-	@Test
-	void test() throws Exception {
-		bib.userRegistrieren("obai", "student", 15, "nein");
-		bib.userAnmelden("K1001");
-		bib.mediumAusleihen("K1001", "B001");
-		
-	}
-
+    /**
+     * Startet eine Ausleihe für einen registrierten und angemeldeten Benutzer.
+     *
+     * @throws Exception bei Fehlern im Ablauf
+     */
+    @Test
+    @DisplayName("Ausleihe starten (ohne Überfälligkeit) läuft fehlerfrei")
+    void ausleiheStartOhneGebuehren() throws Exception {
+        bib.userRegistrieren("obai", "student", 15, "nein");
+        bib.userAnmelden("K1001");
+        assertDoesNotThrow(() -> bib.mediumAusleihen("K1001", "B001"));
+    }
 }
